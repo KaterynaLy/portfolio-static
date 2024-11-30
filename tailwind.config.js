1de0fcc4a14ca10d007cff7a2b66ff769f8b239e
KaterynaLy <katerynachile@gmail.com>
2024-11-30 12:37:06 +0100

    Tailwind is installed and is working properly

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html",     // Archivos HTML en la carpeta public
+    "./public/src/**/*.js",   // Archivos JS en la carpeta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
